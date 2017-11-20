a687ce253bd458b9bcfcde3406cda19afc23de6a
Chez <chez.vang@gmail.com>
2017-11-20 10:42:23 -0600

    cleaned prompts

--- RPSLS/RPSLS/PlayerPick.cs
+++ RPSLS/RPSLS/PlayerPick.cs
@@ -19,7 +19,7 @@ namespace RPSLS
 
         public virtual string PromptChoice()
         {
-            Console.WriteLine("\n Choices: \n 1. Rock \n 2. Paper \n 3. Scissors \n 4. Spock \n 5. Lizard\nChoose your weapon:");
+            Console.WriteLine("\n Choices: \n [1] Rock \n [2] Paper \n [3] Scissors \n [4] Spock \n [5] Lizard\nChoose your weapon:");
             string playerChoice = Console.ReadLine();
             AnnounceDecision(playerChoice);
             return playerChoice;
